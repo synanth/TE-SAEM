27501e9d295b58f80223600cd147983570da2ede
Synanth Texocaelum <texocaelum@gmail.com>
2025-12-17 17:06:41 -0600

    em: read_lens from assembly

--- manuscript/em.py
+++ manuscript/em.py
@@ -5,11 +5,22 @@ import random
 
 ## em ##
 
+#def log_likelihood(theta, len_transcripts, multimapped_reads, read_lens):
+#    log_sum = 0
+#    for read, tes in multimapped_reads.items():
+#        log_sum += math.log(max(sum([theta[te] / max(len_transcripts[te]-read_lens[read]+1,1) for te in tes]), 1e-300))
+#    return log_sum
 def log_likelihood(theta, len_transcripts, multimapped_reads, read_lens):
-    log_sum = 0
+    ll = 0.0
     for read, tes in multimapped_reads.items():
-        log_sum += math.log(max(sum([theta[te] / max(len_transcripts[te]-read_lens[read]+1,1) for te in tes]), 1e-300))
-    return log_sum
+        xs = [
+            math.log(theta[te]) -
+            math.log(max(len_transcripts[te] - read_lens[read] + 1, 1))
+            for te in tes
+        ]
+        m = max(xs)
+        ll += m + math.log(sum(math.exp(x - m) for x in xs))
+    return ll
 
 
 def init_abundance(len_transcripts, multimapped_reads, all_tes):
@@ -27,18 +38,38 @@ def e_step(theta, multimapped_reads, len_transcripts, read_lens):
     frac = {k:{} for k in multimapped_reads}
 
     for read, tes in multimapped_reads.items():
-        frac_sum = 0
+        xs = []
         for te in tes:
-            e_len = max(len_transcripts[te]-read_lens[read]+1,1)
-            frac[read][te] = (theta[te] / e_len) 
-            frac_sum += theta[te] /e_len
-        if frac_sum == 0:
-            for te in tes:
-                frac[read][te] = 1.0/len(tes)
-            continue
-        for te in tes:
-            frac[read][te] /= frac_sum
+            e_len = max(len_transcripts[te] - read_lens[read] + 1, 1)
+            xs.append(math.log(theta[te]) - math.log(e_len))
+
+        m = max(xs)
+        Z = sum(math.exp(x - m) for x in xs)
+
+        for te, x in zip(tes, xs):
+            frac[read][te] = math.exp(x - m) / Z
+
     return frac
+
+
+#def e_step(theta, multimapped_reads, len_transcripts, read_lens):
+#    frac = {k:{} for k in multimapped_reads}
+#    for read, tes in multimapped_reads.items():
+#        frac_sum = 0
+#        for te in tes:
+#            e_len = max(len_transcripts[te]-read_lens[read]+1,1)
+#            frac[read][te] = (theta[te] / e_len) 
+#            if not math.isfinite(theta[te] / e_len):
+#                print("BAD VALUE", read, te, theta[te], e_len)
+#                sys.exit(1)
+#            frac_sum += theta[te] /e_len
+#        if frac_sum == 0:
+#            for te in tes:
+#                frac[read][te] = 1.0/len(tes)
+#            continue
+#        for te in tes:
+#            frac[read][te] /= frac_sum
+#    return frac
 
 
 def m_step(frac, len_transcripts, read_lens, multimapped_reads, all_tes, unique_counts):
@@ -80,7 +111,9 @@ if __name__ == '__main__':
     gtf_loc = base_loc + "refs/hs1.gtf"
     unique_counts_loc = base_loc + "sim_data/star.unique.counts"
     multimapped_loc = base_loc + "sim_data/star.multi.translation"
-    sam_loc = base_loc + "sim_data/alignment/segemehl.sam"
+    #sam_loc = base_loc + "sim_data/alignment/segemehl.sam"
+    sam_loc = base_loc + "sim_data/alignment/star.sam"
+    assembly_loc = base_loc + "sim_data/assembly/to_contigs.sam"
 
     len_transcripts = {}
     unique_counts = {}
@@ -89,6 +122,15 @@ if __name__ == '__main__':
     e_lens = {}
 
     with open(sam_loc, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if line[0] == "@":
+                continue
+            buff = line.strip().split()
+            name = buff[0].split("/")[0]
+            read_lens[name] = len(buff[9])
+    
+    with open(assembly_loc, "r") as f:
         lines = f.readlines()
         for line in lines:
             if line[0] == "@":
@@ -115,7 +157,7 @@ if __name__ == '__main__':
             buff = line.strip().split(",")
             multimapped_reads[buff[0]] = buff[1:]
 
-
+    print(sum(unique_counts.values()))
     em_counts = em(len_transcripts, read_lens, multimapped_reads, unique_counts)
     non_zero = {k:int(v) for k,v in em_counts.items()}
 
