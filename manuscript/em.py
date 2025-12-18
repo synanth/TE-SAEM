7ae5a5de06edafcd3f56363ad10120a913059c31
Synanth Texocaelum <texocaelum@gmail.com>
2025-12-17 19:12:00 -0600

    em: naive gc_bias consideration

--- manuscript/em.py
+++ manuscript/em.py
@@ -4,17 +4,20 @@ import random
 
 
 ## em ##
+def gc_weight(gc):
+    return 1 + 2 * (.5 - gc)
+
 
 #def log_likelihood(theta, len_transcripts, multimapped_reads, read_lens):
 #    log_sum = 0
 #    for read, tes in multimapped_reads.items():
 #        log_sum += math.log(max(sum([theta[te] / max(len_transcripts[te]-read_lens[read]+1,1) for te in tes]), 1e-300))
 #    return log_sum
-def log_likelihood(theta, len_transcripts, multimapped_reads, read_lens):
+def log_likelihood(theta, len_transcripts, multimapped_reads, read_lens, gc):
     ll = 0.0
     for read, tes in multimapped_reads.items():
         xs = [
-            math.log(theta[te]) -
+            math.log(theta[te] * gc_weight(gc[te])) -
             math.log(max(len_transcripts[te] - read_lens[read] + 1, 1))
             for te in tes
         ]
@@ -34,14 +37,14 @@ def init_abundance(len_transcripts, multimapped_reads, all_tes):
     return theta
 
 
-def e_step(theta, multimapped_reads, len_transcripts, read_lens):
+def e_step(theta, multimapped_reads, len_transcripts, read_lens, gc):
     frac = {k:{} for k in multimapped_reads}
 
     for read, tes in multimapped_reads.items():
         xs = []
         for te in tes:
             e_len = max(len_transcripts[te] - read_lens[read] + 1, 1)
-            xs.append(math.log(theta[te]) - math.log(e_len))
+            xs.append(math.log(theta[te] * gc_weight(gc[te])) - math.log(e_len))
 
         m = max(xs)
         Z = sum(math.exp(x - m) for x in xs)
@@ -84,24 +87,24 @@ def m_step(frac, len_transcripts, read_lens, multimapped_reads, all_tes, unique_
     return theta
 
 
-def em(len_transcripts, read_lens, multimapped_reads, unique_counts):
+def em(len_transcripts, read_lens, multimapped_reads, unique_counts, gc):
     threshold = 0.01
     all_tes = list(set([x for sublist in multimapped_reads.values() for x in sublist]))
     old_abundance = init_abundance(len_transcripts, multimapped_reads, all_tes)
-    ll_old = log_likelihood(old_abundance, len_transcripts, multimapped_reads, read_lens)
+    ll_old = log_likelihood(old_abundance, len_transcripts, multimapped_reads, read_lens, gc)
 
     for i in range(1,10000):
-        frac = e_step(old_abundance, multimapped_reads, len_transcripts, read_lens)
+        frac = e_step(old_abundance, multimapped_reads, len_transcripts, read_lens, gc)
         new_abundance = m_step(frac, len_transcripts, read_lens, multimapped_reads, all_tes, unique_counts)
-        ll_new = log_likelihood(new_abundance, len_transcripts, multimapped_reads, read_lens)
+        ll_new = log_likelihood(new_abundance, len_transcripts, multimapped_reads, read_lens, gc)
         diff = ll_new - ll_old
         print(i, diff, ll_old, ll_new)
 
         if abs(diff) < threshold:
-            return {k:v*len(multimapped_reads) for k,v in old_abundance.items()}
+            return {k:v*len(multimapped_reads) for k,v in new_abundance.items()}
         old_abundance = new_abundance
         ll_old = ll_new
-    return {k:v*len(multimapped_reads) for k,v in old_abundance.items()}
+    return {k:v*len(multimapped_reads) for k,v in new_abundance.items()}
 
 
 
@@ -120,6 +123,7 @@ if __name__ == '__main__':
     multimapped_reads = {}
     read_lens = {}
     e_lens = {}
+    gc = {}
 
     with open(sam_loc, "r") as f:
         lines = f.readlines()
@@ -143,7 +147,9 @@ if __name__ == '__main__':
         lines = f.readlines()
         for line in lines:
             buff = line.strip().split()
-            len_transcripts[buff[11][1:-2]] = abs(int(buff[4]) - int(buff[3]))
+            name = buff[11][1:-2] 
+            len_transcripts[name] = abs(int(buff[4]) - int(buff[3]))
+            gc[name] = float(buff[-1][1:-2])
 
     with open(unique_counts_loc, "r") as f:
         lines = f.readlines()
@@ -157,9 +163,9 @@ if __name__ == '__main__':
             buff = line.strip().split(",")
             multimapped_reads[buff[0]] = buff[1:]
 
-    print(sum(unique_counts.values()))
-    em_counts = em(len_transcripts, read_lens, multimapped_reads, unique_counts)
-    non_zero = {k:int(v) for k,v in em_counts.items()}
+#    print(sum(unique_counts.values()))
+    em_counts = em(len_transcripts, read_lens, multimapped_reads, unique_counts,gc)
+    non_zero = {k:int(v) for k,v in em_counts.items() if v > 2}
 
     all_tes = {k:0 for k in len_transcripts}
     for te in all_tes:
